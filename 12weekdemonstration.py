c71b1a50ca86f5755622a6e4729f39c4096bf8d3
HBoldt <81538869+HBoldt@users.noreply.github.com>
2021-04-14 22:10:22 -0400

    Add files via upload

--- 12weekdemonstration.py
+++ 12weekdemonstration.py
@@ -1,6 +1,6 @@
 #Air2AIr
 #Love, Boldt 
-#updated 7apr21
+#updated 14apr21
 
 #Import pyparrot
 from pyparrot.Anafi import Anafi
@@ -42,43 +42,103 @@ while a < 3:
         i = 1
         while i < 9:
             anafi.move_relative(dx=0,dy=0,dz=0,dradians=.7) #clockwise
-
+            
+            #initializations 
+            #count for later
+            count = 0 
+            #JAY/JERRA camera input into script 
+            
+            #JAY/JERRA ATD = April Tag detected? function
+            if ATD == 1:
+            
+                #x_vector = 50 #initialize x vector
+                while (x_vector > 10) or (x_vector < -10): 
+                    #JAY/JERRA x_vector = apriltag x vector function
+                    
+                    #move anafi left or right depending on x
+                    #move right
+                        if x_vector > 10:
+                            #clockwise
+                            anafi.move_relative(dx=0,dy=0,dz=0,dradians=.1) 
+                            
+                            
+                    #move left
+                        if x_vector < -10:
+                           #COUNTER clockwise
+                           anafi.move_relative(dx=0,dy=0,dz=0,dradians=-.1) 
+                          
+                    
+                    
+                #y_vector = 50 #initialize y vector
+                while (y_vector > 10) or (y_vector < -10): 
+                    #JAY/JERRA y_vector = apriltag y vector function
+                    #move anafi up or down depending on y
+                    #move up
+                        if y_vector > 10:
+                             anafi.fly_direct(0,0,0,5,1) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
+                            
+                    #move down
+                        if y_vector < -10:
+                           anafi.fly_direct(0,0,0,-5,1) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
+                                        
+                while (ATD != 0) :
+                    #JAMES move forward function
+                    anafi.fly_direct(0,40,0,0,.5) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
+                    #JERRA / JAY april tag detection function
+                    
+                    #count loop number for return
+                    count += 1
+                    
+            #Drone moves back number of interations of count
+            count2 = 0 
+            while (count2 != count):
+                #JAMES move drone backward
+                anafi.fly_direct(0,-40,0,0,.5) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
+                count2 += 1
+            
+            #finish the spin loop 
+            
+            
+            ###################
+            #Notes
+            ###################    
+            #Pass data in form .mp4 or webcam
             #Video Detection Condition
-            cv = 0 # video.videofunc()
+            #cv = 0 # video.videofunc()
 
 
-            if cv == "forward":
-                anafi.fly_direct(0,40,0,0,2) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
-                anafi.fly_direct(0,-40,0,0,1.9) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
-                print("Strafing forward")
-            elif cv == "backward":
-                anafi.fly_direct(0,-40,0,0,2) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
-                anafi.fly_direct(0,40,0,0,1) #fly_direct(roll, pitch, yaw, vertical_movement, duration)   
-                print("Strafing backward")
-            elif cv == "sleft":
-                 anafi.fly_direct(-40,0,0,0,2) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
-                 anafi.fly_direct(40,0,0,0,1) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
-                 print("Strafing right")
-            elif cv == "sright":
-                 anafi.fly_direct(40,0,0,0,2) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
-                 anafi.fly_direct(-40,0,0,0,1) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
-                 print("Strafing left")
-            elif cv == "left":
-                anafi.fly_direct(0,0,0,40,2) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
-                anafi.fly_direct(0,0,0,-40,1) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
-                print("Turning right")
-            elif cv == "right":
-                anafi.fly_direct(40,0,0,-40,2) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
-                anafi.fly_direct(-40,0,0,40,1) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
-                print("Turning left")
-            elif cv == "up":
-                anafi.fly_direct(0,0,0,40,2) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
-                anafi.fly_direct(0,0,0,-40,1) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
-                print("Ascending")
-            elif cv == "down":
-                anafi.fly_direct(0,0,0,-40,2) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
-                anafi.fly_direct(0,0,0,40,1) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
-                print("Decending") 
+            #if cv == "forward":
+            #    anafi.fly_direct(0,40,0,0,2) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
+            #    anafi.fly_direct(0,-40,0,0,1.9) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
+            #    print("Strafing forward")
+            #elif cv == "backward":
+            #    anafi.fly_direct(0,-40,0,0,2) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
+            #    anafi.fly_direct(0,40,0,0,1) #fly_direct(roll, pitch, yaw, vertical_movement, duration)   
+            #    print("Strafing backward")
+            #elif cv == "sleft":
+            #     anafi.fly_direct(-40,0,0,0,2) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
+            #     anafi.fly_direct(40,0,0,0,1) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
+            #     print("Strafing right")
+            #elif cv == "sright":
+            #     anafi.fly_direct(40,0,0,0,2) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
+            #     anafi.fly_direct(-40,0,0,0,1) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
+            #     print("Strafing left")
+            #elif cv == "left":
+            #    anafi.fly_direct(0,0,0,40,2) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
+            #    anafi.fly_direct(0,0,0,-40,1) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
+            #    print("Turning right")
+            #elif cv == "right":
+            #    anafi.fly_direct(40,0,0,-40,2) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
+            #    anafi.fly_direct(-40,0,0,40,1) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
+            #    print("Turning left")
+            #elif cv == "up":
+            #    anafi.fly_direct(0,0,0,40,2) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
+            #    anafi.fly_direct(0,0,0,-40,1) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
+            #    print("Ascending")
+            #elif cv == "down":
+            #    anafi.fly_direct(0,0,0,-40,2) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
+            #    anafi.fly_direct(0,0,0,40,1) #fly_direct(roll, pitch, yaw, vertical_movement, duration)
+            #    print("Decending") 
                 
 
 
---
+++ instanceloop.py
@@ -0,0 +1,55 @@
+# import the necessary packages
+import pupil_apriltags
+import cv2
+import time
+
+def rescale(frame, percent=60):
+    scale_percent = 60
+    width = int(frame.shape[1]*scale_percent/100)
+    height = int(frame.shape[0]*scale_percent/100)
+    dim = (width, height)
+    return cv2.resize(frame,dim, interpolation = cv2.INTER_AREA)
+
+cam = cv2.VideoCapture(0)
+
+#set camera resolution
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640*2)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480*2)
+print("Current camera resolution: (" + str(cam.get(3)) + "; " + str(cam.get(4)) + ")")
+
+# create AprilTags detector with options
+detector = pupil_apriltags.Detector(families="tag36h11")
+
+frame_number=0
+
+# keep looping
+results = []
+while results == []:
+
+    if not cam.isOpened():
+            cam.open(0)
+            print('initializing...')
+    (grabbed, frame) = cam.read()
+    image = rescale(frame, percent=50)
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # execute the detector to find the tags (more options exist)
+    results = detector.detect(gray, estimate_tag_pose=False, camera_params=None, tag_size=0.17)
+    print(results)
+    #print("{} AprilTag(s) detected".format(len(results)))
+
+##
+#
+	# show the frame to our screen and increment the frame
+    cv2.imshow("April Tag Detector", image)
+
+    # if the 'q' key is pressed, stop the loop
+    key = cv2.waitKey(1) & 0xFF
+    if key == 27 or key == ord("q"):
+            break
+    frame_number=frame_number + 1
+
+# release the camera and close any open windows
+cam.release()
+cv2.destroyAllWindows()
